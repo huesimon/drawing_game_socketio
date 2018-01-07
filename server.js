f74dcc7249bd7f6d174e268524b00b7c319d0c7b
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-01-08 00:55:51 +0100

    adding connections to an array, removes them when they disconnect...

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var server = app.listen(3000);
+var playerList = [];
 
 app.use(express.static('public'));
 
@@ -11,20 +12,51 @@ var io = socket(server);
 
 
 io.sockets.on('connection', newConnection);
+// io.sockets.on('disconnect', newConnection );
 
 
 
 function newConnection(socket) {
-    console.log('new connection ' + socket.id)
+    console.log('new connection ' + socket.id);
+    socket.on('disconnect', userDisconnect);
+        //add connected player to the list
+        //idea is to let one user at the time draw, hopefully
+        playerList.push(socket.id);
 
 
-    //if there  is a message called mouse, trigger function "mouseMsg"
-    socket.on('mouse', mouseMsg);
+        //if there  is a message called mouse, trigger function "mouseMsg"
+        socket.on('mouse', mouseMsg);
 
-    function mouseMsg(data) {
-        socket.broadcast.emit('mouse', data);
-        //io.sockets.emit('mouse', data);
-        //will send to everyone + you
-        console.log(data)
+        function mouseMsg(data) {
+            socket.broadcast.emit('mouse', data);
+            //io.sockets.emit('mouse', data);
+            //will send to everyone + you
+            console.log(data);
+            console.log(playerList.length);
+        }
+
+    function userDisconnect() {
+            //for some reason i don't have to use socket as a parameter, tried doing that for too long ZzzZ...
+        //socket was already a parameter in "newConnection"...
+        console.log("User disconnected" + socket.id);
+        for (var i = 0 ; i < playerList.length; i++) {
+            if (playerList[i] === socket.id) {
+                playerList.splice(i, 1);
+            }
+
+        }
+
+
     }
+
+    function whoIsAllowedToDraw() {
+        //pick a player that is allowed to draw
+        //should it be random? or just the first player in the array?
+        //what if player leaves?
+        //pick next player in array
+
+    }
+
+
 }
+
