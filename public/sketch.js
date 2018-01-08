5504ebbd15c680e1f1055d0c15bd3b38bac378fa
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-01-08 02:16:29 +0100

    Only one players drawing will get shown. sketch.js (mouseDragged needs some clean up, code is F#$£3D)

--- public/sketch.js
+++ public/sketch.js
@@ -4,6 +4,7 @@ var input, red, green, blue;
 
 var hintText = "This is the hint";
 var answerText;
+var playerAllowedToDraw = "N/A";
 
 function setup() {
     r = 255;
@@ -34,11 +35,13 @@ function setRed() {
     g = 0;
     b = 0;
 }
+
 function setGreen() {
     r = 0;
     g = 255;
     b = 0;
 }
+
 function setBlue() {
     r = 0;
     g = 0;
@@ -46,6 +49,9 @@ function setBlue() {
 }
 
 function newDrawing(data) {
+    playerAllowedToDraw = data.id;
+
+
     noStroke();
     fill(data.red, data.green, data.blue);
     ellipse(data.x, data.y, 30, 30);
@@ -53,23 +59,58 @@ function newDrawing(data) {
 }
 
 function mouseDragged() {
-    noStroke();
-    fill(r, g, b);
-    ellipse(mouseX, mouseY, 30, 30);
-    console.log('Sending: ' + mouseX + ', ' + mouseY);
+console.log("your id: " + socket.id + " id that is allowed to draw: " + playerAllowedToDraw);
+
+    if(canIDraw(playerAllowedToDraw)){
+        noStroke();
+        fill(r, g, b);
+        ellipse(mouseX, mouseY, 30, 30);
+        console.log('Sending: ' + mouseX + ', ' + mouseY + "," + socket.id);
+
+        var data = {
+            x: mouseX,
+            y: mouseY,
+            red: r,
+            green: g,
+            blue: b,
+            id: socket.id
+        };
+        socket.emit('mouse', data);
+    }
+
+    //Not drawing any ellipse, just sending data, so server.js can check it()
+    // noStroke();
+    // fill(r, g, b);
+    // ellipse(mouseX, mouseY, 30, 30);
+    console.log('Sending: ' + mouseX + ', ' + mouseY + "," + socket.id);
 
     var data = {
         x: mouseX,
         y: mouseY,
         red: r,
         green: g,
-        blue: b
+        blue: b,
+        id: socket.id
     };
     socket.emit('mouse', data);
 }
 
 function draw() {
     textSize(32);
-    fill(0,0,0);
-    text(hintText, width/2 ,35);
+    fill(0, 0, 0);
+    text(hintText, width / 2, 35);
+}
+
+function canIDraw(id) {
+    //Is my id the same as the first person that joined?
+    //server.js has an array, and a function called "whoIsAllowedToDraw"
+    //It will broadcast the drawing, and ignore all other socket connections input
+    //canIDraw will hopefully ignore the other users input to the canvas
+    //so that you won't destroy the masterpiece of the "drawer"
+
+    //if i'm the first person in the array
+    if (socket.id === id) {
+        return true;
+
+    } else return false;
 }
